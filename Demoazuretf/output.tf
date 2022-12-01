3451d501d3ac7b2cc5d828376e4dbefce9c331b4
srijabh <venkatasrijabhupathiraju.origin@gmail.com>
2022-11-30 17:44:33 -0800

    added output.tf

---
+++ Demoazuretf/output.tf
@@ -0,0 +1,11 @@
+output "rg1id" {
+  value = "azurerm_resource_group.rg1.id"
+}
+
+output "rg2id" {
+  value = "azurerm_resource_group.rg2.id"
+}
+
+output "rg3id" {
+  value = "azurerm_resource_group.rg3.id"
+}
